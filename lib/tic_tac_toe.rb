37251d7e956827bae15339c3bb540a769ddbd330
Austin Buhler <austinbuhler@gmail.com>
2020-03-15 18:15:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,3 +45,22 @@ def turn(board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+   turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+     "X"
+  else
+     "O"
+  end
+end
